0ade4ad8509afc4dc775b4938e0c5e9f4eb12d80
Muhammad Ahsan Nasir <294302954+ahsangamedev@users.noreply.github.com>
2026-09-01 19:04:26 +0500

    Initial check-in

---
+++ Assets/Scriipts/CarController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+
+
+
+    void Move()
+    {
+        transform.position += transform.forward * moveSpeed * Time.deltaTime; // the pos z axis
+    }
+
+
+
+}
